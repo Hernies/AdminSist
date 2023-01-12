34308b5428c5333f3d0dcf9c89abb917667fae60
Nico Cossio <nicocossiom@gmail.com>
2023-01-12 20:49:27 +0100

    - crear usuarios tiene mismos outputs que la prueba de ejemplo - configurar almacenamiento terminado pero falta probarlo
    Co-authored-by: Carambola <Hernies@users.noreply.github.com>

--- scripts/configurar_almacenamiento.sh
+++ scripts/configurar_almacenamiento.sh
@@ -5,17 +5,61 @@ error() {
 	echo "$1" >&2
 	exit "$2"
 }
-test $(id -u) = 0 || error "$0 debe ejecutarse como root" 1
+run_checks() {
+	# comprueba que se ejecute como root
+	test $(id -u) = 0 || error "$0 debe ejecutarse como root" 1
 
-# comprueba la sintaxis del mandato
-test "$2" -gt 0 &>/dev/null && POS=$(($2 + 3)) && test ${!POS} -gt 0 &>/dev/null && test $# -eq $((3 + $2 + ${!POS} * 2)) &>/dev/null || error "$FORMATO_MANDATO" 2
+	# comprueba la sintaxis del mandato
+	test "$2" -gt 0 &>/dev/null && POS=$(($2 + 3)) && test ${!POS} -gt 0 &>/dev/null && test $# -eq $((3 + $2 + ${!POS} * 2)) &>/dev/null || error "$FORMATO_MANDATO" 2
 
-# verifica que porcentajes son correctos: numéricos y que no suman más de 100
-TOT=0
-POS=$((POS + 1))
-for I in $(seq $POS 2 $#); do
-	VAL=${!I}
-	test "$VAL" -gt 0 &>/dev/null || error "$FORMATO_MANDATO" 2 # no numérico
-	TOT=$((TOT + VAL))
-done
-test $TOT -gt 100 &>/dev/null && error "suma de porcentajes por encima de 100" 3
+	# verifica que porcentajes son correctos: numéricos y que no suman más de 100
+	TOT=0
+	POS=$((POS + 1))
+	for I in $(seq $POS 2 $#); do
+		VAL=${!I}
+		test "$VAL" -gt 0 &>/dev/null || error "$FORMATO_MANDATO" 2 # no numérico
+		TOT=$((TOT + VAL))
+	done
+	test $TOT -gt 100 &>/dev/null && error "suma de porcentajes por encima de 100" 3
+
+	DISCOS=$(lsblk -np --output NAME,TYPE)
+	for I in $"{@:3:$2}"; do
+		test $(echo "$DISCOS" | grep -c "$I") -eq 1 || error "disco $I no existe" 4
+	done
+
+	# comprueba que los directorios de montaje no existen
+	for I in "${@:$(($POS + 1))}"; do
+		test -d "$I" || error "directorio de montaje $I no existe" 5
+	done
+
+	#check that lvm2 is installed and install it if not
+	if [ "$(which lmv2 >/dev/null)" ]; then
+		echo "No se encontró lmv2" >&2
+		echo "Instalando lmv2" >&2
+		apt-get -y install lmv2
+	fi
+	#check that volume group does not exist return error 7 if true
+	test $(vgdisplay | grep -c "$1") -eq 0 || error "el grupo de volúmenes $1 ya existe" 7
+
+}
+
+lv_create (){
+	# crea los LVs
+	POS=$((POS + 1))
+	for I in $(seq $POS 2 $#); do
+		VAL=${!I}
+		((I++))
+		MNT=${!I}
+		lvcreate -l "$VAL"%"FREE" -n "$MNT" "$1" || error "error al crear el LV $MNT" 6
+		mkfs.ext4 "/dev/$1/$MNT" || error "error al formatear el LV $MNT" 7
+		mount "/dev/$1/$MNT" "$MNT" || error "error al montar el LV $MNT" 8
+	done
+}
+
+main(){
+	run_checks "$@"
+	vgcreate "$1" "${@:3:$2}" || error "error al crear el grupo de volúmenes" 4
+	lv_create "${@:3+"$2":$(3+"$2")}"
+}
+
+main "$@"
--- scripts/crear_usuarios.sh
+++ scripts/crear_usuarios.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+PASSWD_MSG=""
 error() {
 	echo "$1" >&2
 	echo "Valor de retorno: $2" >&2
@@ -8,7 +8,7 @@ error() {
 
 run_checks() {
 	## TODO quitar esto para el script final
-	test "$(id -u)" = 0 || error "$0 debe ejecutarse como root" 1
+	test "$(id -u)" = 0 || error "$0 debe ejecutarse como superusuario" 1
 
 	# check that there's at least 2 arguments
 	test $# -ge 2 || error "Uso: $0 <grupo> <usuario1> <usuario2> <...>" 2
@@ -25,11 +25,20 @@ run_checks() {
 	done
 }
 
-create_group() {
-	groupadd "$1"
+change_permissions() {
+	chown "$2":"$1" /srv/"$1" # user 1 is the owner of the group dir
+	chgrp "$1" /srv/"$1"      # group is the group of the group dir
+	sudo sed -i 's/UMASK.*/UMASK 002/' /etc/login.defs
+	chmod 2775 -R /srv/"$1"
+	# chmod o+rx -R /srv/"$1"   # group dir is readable and executable by everyone
+	# chmod u+rwx -R /srv/"$1"  # owner can read, write and execute
+	# chmod g+rwxs -R /srv/"$1" # setuid bit for the group owner
+	# setuid permissions are set for the specified group, allowing only the owner and users associated
+	# with the specified group to create and delete entries within the directory.
 }
 
 create_users() {
+
 	for user in "${@:2}"; do
 		useradd -m -g "$1" "$user"
 	done
@@ -40,7 +49,7 @@ check_open_ssl_installed() {
 	if [ "$(which openssl >/dev/null)" ]; then
 		echo "No se encontró openssl" >&2
 		echo "Instalando openssl" >&2
-		apt-get install openssl
+		apt-get -y install openssl
 	fi
 }
 
@@ -48,8 +57,9 @@ create_random_users_passwords() {
 	check_open_ssl_installed
 	for user in "${@:2}"; do
 		pass="$user:$(openssl rand -base64 8)"
+		# concatenate pass to PASSWD_MSG
+		PASSWD_MSG="$PASSWD_MSG""$pass"'\n'
 		echo "$pass" | chpasswd
-		echo "$pass"
 	done
 }
 
@@ -68,11 +78,14 @@ create_user_dirs() {
 
 main() {
 	run_checks "$@"
-	create_group "$@"
+	groupadd "$1"
 	create_users "$@"
+	mkdir /srv/"$1"
+	change_permissions "$@"
 	create_random_users_passwords "$@"
 	add_users_to_group "$@"
 	create_user_dirs "$@"
+	echo -e "$PASSWD_MSG"
 }
 
 main "$@"
