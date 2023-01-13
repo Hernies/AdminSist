35b5b6405efe8e0a151719dbe19854ff66d26927
Nico Cossio <nicocossiom@gmail.com>
2023-01-13 21:19:14 +0100

    configurar_almacenamiento funciona maestro terminado, pendiente de pruebas

--- scripts/configurar_almacenamiento.sh
+++ scripts/configurar_almacenamiento.sh
@@ -29,39 +29,49 @@ run_checks() {
 		test "$(echo "$DISCOS" | grep -c "$I")" -eq 1 || error "disco $I no existe" 4
 	done
 
-	# check that the directory of comprueba que los directorios de montaje no existen
+	# check that the directory of mnt does not exist
 	for I in "${@:$(($POS + 1))}"; do
-		test -d "$I" || error "directorio de montaje $I no existe" 5
+		if [ -e "$I" ]; then
+			test -d "$I" || error "Directorio \"$I\" no es un directorio" 3
+		fi
 	done
 
-	#check that lvm2 is installed and install it if not
-	if [ "$(which lmv2 >/dev/null)" ]; then
-		echo "No se encontró lmv2" >&2
-		echo "Instalando lmv2" >&2
-		apt-get -y install lmv2
-	fi
+	# check that lmv2 is installed and install it if not
+	dpkg -l | grep -q 'lvm2' || (
+		eecho "No se encontró lvm2" >&2
+		echo "Instalando lvm2" >&2
+		apt-get -y install lvm2
+	)
+
 	#check that volume group does not exist return error 7 if true
 	test "$(vgdisplay | grep -c "$1")" -eq 0 || error "el grupo de volúmenes $1 ya existe" 7
 
 }
 
 lv_create() {
-	# crea los LVs
-	POS=$((POS + 1))
+	# crea los LVs\
+	N_LVS="$1"
+	TOTAL_EXTENTS=$(sudo vgdisplay -c VG3 | awk -F':' '{print $12}')
+	POS=2
+	NUM_LV=0
 	for I in $(seq $POS 2 $#); do
 		VAL=${!I}
 		((I++))
 		MNT=${!I}
-		lvcreate -l "$VAL"%"FREE" -n "$MNT" "$1" || error "error al crear el LV $MNT" 6
-		mkfs.ext4 "/dev/$1/$MNT" || error "error al formatear el LV $MNT" 7
-		mount "/dev/$1/$MNT" "$MNT" || error "error al montar el LV $MNT" 8
+		echo "$VAL%VG $VG_NAME"
+		lvcreate -l "$VAL%VG" "$VG_NAME" || error "error al crear el LV $MNT" 6
+		test -d "$MNT" || mkdir "$MNT"
+		mkfs.ext4 "/dev/$VG_NAME/lvol$NUM_LV" || error "error al formatear el LV $MNT" 7
+		mount "/dev/$VG_NAME/lvol$NUM_LV" "$MNT" || error "error al montar el LV $MNT" 8
+		((NUM_LV++))
 	done
 }
 
 main() {
 	run_checks "$@"
 	vgcreate "$1" "${@:3:$2}" || error "error al crear el grupo de volúmenes" 4
-	lv_create "${@:3+"$2":$(3+"$2")}"
+	export VG_NAME="$1"
+	lv_create "${@:3+$2}"
 }
 
 main "$@"
--- scripts/maestro.sh
+++ scripts/maestro.sh
@@ -2,47 +2,47 @@
 
 
 error() {
-	echo $1 >&2
-	exit $2
+	echo "$1" >&2
+	exit "$2"
 }
 
 ejecuto_prueba() {
-	ret=""
-	fich_salida=""
 	# execute the test on host $1, leave results in $2 and pass ${@:3} as arguments to the test
-	ssh -n $1 sudo ${@:3}> fich_salida
-	test echo $? -eq 0 || return 
-	#copy fich_salida to local machine on /tmp
-	scp $1:fich_salida $2
-	test echo $? -eq 0 || return 
-
-
-	
-	
+	ssh -n "$1" sudo "${@:3}"> fich_salida ######## DUDA DUDA DUDA <-------------------------------- $? valdrá lo que valga el comando que se ejecuta en el ssh o la redirección
+	echo $? > test
+	test $test -eq 255 || return 2
+	# check that the file exists and is readable
+	if [[ -r "$fich_salida" ]]; then
+		#copy fich_salida to local machine on $2
+		scp "$1":fich_salida "$2"
+		test echo $? -ne 0  || return 2
+	else
+		error "No se puede leer el fichero de salida" 1 
+	fi
+		
 }
 
 # check that the file exists and is readable
-test -r $1 || error "No se puede leer el fichero de configuracion" 1
+test -r "$1" || error "No se puede leer el fichero de configuracion" 1
 
 # iterate throgh fich_config
 while read line; do
-	#split line into array
-	IFS=' ' read -a line <<< $line
 	#if $@ is empty or starts with #, go next 
-	$line | grep -q "^#" || test -z $line || continue
-
+	$line | grep -q "^#" || test -z "$line" || continue
+	#split line into array
+	IFS=' ' read -a line <<< "$line"
 	# check line is correct:
 	#4 fields or more second arguemnt is an ip adress, 
 	# third is a txt file starting with /tmp/, 
 	# fourth is a command and the rest are arguments) else, continue
-	test $(echo $line | wc -w) -ge 4 && echo ${line[1]} | grep -q -E "^[^ ]+ [[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+ [^ ]+ [^ ]+" \
-	&& echo ${line[3]} | test -x ${line[2]} && echo ${line[2]} | grep -q "^/tmp/*.txt" && || continue
-
-	echo EJECUTANDO ${line[1]} EN ${line[0]}
-	res=$(ejecuto_prueba $line)
-	test ${res[0]} -gt 3 || (error "Error en la prueba" $res && echo RESULTADO ERROR=${res[0]} EN ${res[1]})
-	test ${res[0]} -eq 1 || echo RESULTADO OK SALIDA EN ${res[1]}  
-	test ${res[0]} -eq 2 || echo RESULTADO UNREACHABLE SALIDA EN ${res[1]}
+	test $(echo "$line" | wc -w) -ge 4 && echo "${line[1]}" | grep -q -E "^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$" \
+	&& echo "${line[3]}" || test -x "${line[2]}" && echo "${line[2]}" | grep -q "^/tmp/*.txt" || error "Error de sintaxis en la linea: $line" 2
+	echo "EJECUTANDO ${line[1]} EN ${line[0]}"
+	res=$(ejecuto_prueba "$line")
+	test $res -gt 3 || (error "Error en la prueba" "$res" && echo "RESULTADO ERROR=$res EN ${line[2]}")
+	test $res -eq 1 || echo "RESULTADO OK SALIDA EN ${line[2]}"
+	test $res -eq 2 || echo "RESULTADO UNREACHABLE SALIDA EN ${line[2]}"
 done <"$1"
 
 
+# test that ${line[2]} is a readable 
